0adf7d7efb4cf4be3e057b5adabe2b251a667e20
Payal-mak <iampayal018@gmail.com>
2025-07-04 23:37:00 +0530

    chore: import message

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import {useParams} from "react-router-dom";
+import { blog_data } from '../assets/assets';
+import { div } from 'motion/react-client';
+import Navbar from '../components/Navbar';
 
 const Blog = () => {
-  return (
+
+  const {id} = useParams()
+  const [data,setData] = useState(null)
+  const fetchBlogData = async () => {
+    const data = blog_data.find(item => item._id === id)
+    setData(data)
+  }
+
+  useEffect(()=>{
+    fetchBlogData()
+  },[])
+  
+  return data ? (
+
+    <div className='relative'>
+      <img src={assets.gradientBackground} alt='' className='absolute -top-50 -z-1 opacity-50'/>
+    <Navbar/>
+
     <div>
-      <h1>Blog</h1>
+      <p>Published on </p>
     </div>
-  )
+    <div></div>
+
+    </div>
+  ) : <div>Loading...</div>
 }
 
 export default Blog
